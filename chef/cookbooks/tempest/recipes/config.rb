1f9ebda56377c78e62f3d4687067ea8e59ff2676
Vincent Untz <vuntz@suse.com>
2015-04-27 14:05:19 +0200

    Disable interface_attach tests for docker
    This is not implemented in the docker driver, so can't work.
    
    (cherry picked from commit 76e0e1102812f59e59a455020805dba475ec70fd)

--- chef/cookbooks/tempest/recipes/config.rb
+++ chef/cookbooks/tempest/recipes/config.rb
@@ -366,9 +366,11 @@ EOH
   end
 
   use_docker = true
+  use_interface_attach = false
   use_rescue = false
 else
   use_docker = false
+  use_interface_attach = true
   use_rescue = true
 end
 
@@ -418,6 +420,7 @@ template "#{tempest_conf}" do
     :flavor_ref => flavor_ref,
     :alt_flavor_ref => alt_flavor_ref,
     :nova_api_v3 => nova[:nova][:enable_v3_api],
+    :use_interface_attach => use_interface_attach,
     :use_rescue => use_rescue,
     :use_resize => use_resize,
     :use_livemigration => use_livemigration,
