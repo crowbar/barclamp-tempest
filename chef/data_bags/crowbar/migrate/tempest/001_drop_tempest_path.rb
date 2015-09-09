c02d0a843f36269d32f09e853d174550ec768408
Vincent Untz <vuntz@suse.com>
2015-09-09 11:31:27 +0200

    Add note that this got moved to merged repo

--- chef/data_bags/crowbar/migrate/tempest/001_drop_tempest_path.rb
+++
@@ -1,9 +0,0 @@
-def upgrade ta, td, a, d
-  a.delete 'tempest_path'
-  return a, d
-end
-
-def downgrade ta, td, a, d
-  a['tempest_path'] = '/opt/tempest'
-  return a, d
-end
--- chef/roles/tempest.rb
+++
@@ -1,8 +0,0 @@
-name "tempest"
-description "Tempest Role - does tempest installation"
-run_list(
-        "recipe[tempest::install]",
-        "recipe[tempest::config]"
-)
-default_attributes()
-override_attributes()
